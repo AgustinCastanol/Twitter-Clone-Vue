d202a7109f8952187e0b2b71050a52831b64305c
AgustinCastanol <acastanol@fi.uba.ar>
2022-03-24 02:11:47 -0300

    Layout created
    First push in repository. This have a layout of twitter clone

---
+++ src/components/NavBar.vue
@@ -0,0 +1,87 @@
+<template>
+  <nav class="lg:w-1/5 border-r border-lighter px-2 lg:px-6 py-2 flex flex-col justify-between">
+        <div>
+            <button
+            class="h-12 w-12 hover:bg-lightblue text-3xl rounded-full text-blue"
+            >
+                <i class="fab fa-twitter"></i>
+            </button>
+            <div>
+                <button
+                v-for="tab in tabs"
+                @click="id = tab.id"
+                :key="tab.id"
+                :class="`
+                focus:outline-none hover:text-blue flex items-center py-2 px-4 hover:bg-lightblue rounded-full mr-auto mb-3 
+                ${id == tab.id ? 'text-blue' : ''
+                }`"
+                >
+                    <i
+                    :class="`${tab.icon} 
+                    text-2xl mr-4 text-left`"
+                    ></i>
+                    <p class="text-lg font-semibold text-left hidden lg:block">{{ tab.title }}</p>
+                </button>
+            </div>
+            <button class="text-white bg-blue rounded-full font-semibold focus:outline-none w-12 lg:h-auto lg:w-full p-3 hover:ng-darkblue">
+            <p class="hidden lg:block">Tweet</p>
+            <i class=" fas fa-plus lg:hidden"></i>
+            </button>
+        </div>
+
+        <div class="w-full relative">
+    
+            <button @click="dropdown =  true" 
+            class="flex items-center w-full hover:bg-lightblue rounded-full p-3focus:outline-none">
+                <img src="profile.png" class="w-10 h-10 rounded-full border border-lighter"/>
+                <div class="hidden lg:block ml-4 ">
+                    <p class="text-sm font-bold leading-tight">Agustin Castanol</p>
+                    <p class="text-sm leading-tight">@agustinCas</p>
+                </div>
+                <i class=" fas fa-angle-down ml-auto text-lg hidden lg:block"></i>
+            </button>
+            <div v-if="dropdown === true" 
+            class="absolute bottom-0 left-0 w-64 rounded-lg shadow-md border-lightest bg-white mb-16">
+                <button @click="dropdown = false"
+                 class=" p-3 flex items-center w-full hover:bg-lightest focus:outline-none">
+                    <img src="profile.png" class="w-10 h-10 rounded-full border border-lighter"/>
+                    <div class="ml-4 ">
+                        <p class="text-sm font-bold leading-tight">Agustin Castanol</p>
+                        <p class="text-sm leading-tight">@agustinCas</p>
+                    </div>
+                    <i class=" fas fa-check ml-auto text-blue"></i>
+                </button>
+                <button @click="dropdown = false"
+                 class="w-full text-left hover:bg:ligtest border-t border-lighter p-3 test-sm ">
+                    Agregar usuario existente
+                </button>
+                <button  @click="dropdown = false"
+                class="w-full text-left hover:bg:ligtest border-t border-lighter p-3 test-sm ">
+                    Cerrar sesión @agustinCas
+                </button>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<script>
+export default {
+    name: 'nav-bar-view',
+    data (){
+        return{
+          tabs: [
+        { icon: "fas fa-home", title: "Inicio", id: "home" },
+        { icon: "fas fa-hashtag", title: "Explorar", id: "explore" },
+        { icon: "far fa-bell", title: "Notificaciones", id: "notifications" },
+        { icon: "far fa-envelope", title: "Mensajes", id: "messages" },
+        { icon: "far fa-bookmark", title: "Guardados", id: "bookmarks" },
+        { icon: "fas fa-clipboard-list", title: "Listas", id: "lists" },
+        { icon: "far fa-user", title: "Perfiles", id: "profile" },
+        { icon: "fas fa-ellipsis-h", title: "Más opciones", id: "more" },
+      ],
+      id: "home",
+      dropdown: false
+    }
+    }
+}
+</script>
---
+++ src/components/Trending.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class=" md:block hidden w-1/3 h-full border-l border-lighter py-2 px-6 overflow-y-scroll relative ">
+    <input class="pl-12 rounded-full w-full p-2 bg-lighter text-sm mb-4" placeholder="Search Twitter">
+    <i class="fas fa-search absolute left-0 top-0 mt-4 ml-10 text-sm text-light "></i>
+    <div class="w-full rounded-lg bg-lightest">
+        <div class="flex items-center justify-between p-3">
+            <p class="text-lg font-bold">Qué está pasando</p>
+            <i class="fas fa-cog text-lg text-blue"></i>
+        </div>
+        <button v-for="trend in trending" :key="trend.id" class="w-full flex justify-between hover:bg-lighter p-3 border-t border-lighter">
+            <div>
+                <p class=" text-sm text-left leading-tight text-dark">{{trend.top}}</p>
+                <p class="font-bold text-sm text-left leading-tight">{{trend.title}}</p>
+                <p class=" text-left leading-tight text-dark">{{trend.bottom}}</p>
+            </div>
+            <i class=" fas fa-angle-down text-lg text-dark"></i>
+        </button>
+        <button class="p-3 w-full hover:bg-lighter text-left text-blue border-t border-lighter">
+        Mostrar más
+        </button>
+    </div>
+     <div class=" md:block hidden  w-full rounded-lg bg-lightest my-4">
+        <div class=" p-3">
+            <p class="text-lg font-bold">A Quién seguir</p>
+            <i class="fas fa-cog text-lg text-blue"></i>
+        </div>
+        <button v-for="friend in friends" :key="friend.id"  class="w-full flex hover:bg-lighter p-3 border-t border-lighter">
+            <img :src="`${friend.src}`" class="w-12 h-12 rounded-full border border-lighter"/>
+            <div class="hidden lg:block ml-4 ">
+                <p class="text-sm font-bold leading-tight"> {{ friend.name }} </p>
+                <p class="text-sm leading-tight"> {{ friend.handle }} </p>
+            </div>
+            <button class="ml-auto text-sm text-blue py-1 px-3 rounded-full border-2 border-blue">
+                Seguir
+            </button>
+        </button>
+        <button class="p-3 w-full hover:bg-lighter text-left text-blue border-t border-lighter">
+        Ver mas
+        </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'trending-section',
+    data (){ 
+        return {
+            trending: [
+                {id: 1 ,top: 'Trending in TX', title: 'Gigi', bottom: 'Trending with: Rip Gigi'},
+                {id: 2 ,top: 'Music', title: 'We Won', bottom: '135K Tweets'},
+                {id: 3 ,top: 'Pop', title: 'Blue Ivy', bottom: '40k tweets'},
+                {id: 4 ,top: 'Trending in US', title: 'Denim Day', bottom: '40k tweets'},
+                {id: 5 ,top: 'Trending', title: 'When Beyonce', bottom: '25.4k tweets'},
+            ],    
+            friends: [
+                {id: 1 ,src: 'elon.jpg',name: 'Elon Musk', handle: '@teslaBoy'},
+                {id: 2 ,src: 'monk.jpg',name: 'Adrian Monk', handle: '@detective:)'},
+                {id: 3 ,src: 'kevin.jpg',name: 'Kevin Hart', handle: '@miniRock'}
+            ]
+        }
+    }
+
+}
+</script>
